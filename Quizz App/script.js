f4a1bff24eb0b7996d07cba76aa605548935cc96
Mohamed-Hema <mohamedhema2017@gmail.com>
2021-01-02 16:53:28 +0200

    quiz app is done

--- Quizz App/script.js
+++ Quizz App/script.js
@@ -34,7 +34,8 @@ const quizData = [{
     d: 'None of the Above',
     correct: 'b'
 }];
-
+const answersEls = document.querySelectorAll('.answer');
+const quiz = document.getElementById('quiz');
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -44,10 +45,12 @@ const submitBtn = document.getElementById('submit');
 
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
     questionEl.innerHTML = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
@@ -56,12 +59,41 @@ function loadQuiz() {
     d_text.innerText = currentQuizData.d;
 }
 
+function getSelected() {
+
+    let answer = undefined;
+    answersEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answersEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
 submitBtn.addEventListener("click", () => {
-    currentQuiz++;
-    if (currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        // To DO: Show results
-        alert('You finished!');
+
+    const answer = getSelected();
+
+    console.log(answer);
+    if (answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML =`<h2>You answered correctly at ${score}/${quizData.length} questions</h2> 
+            
+            <button onclick="location.reload()">Reload</button>`;
+        }
     }
+   
 });
